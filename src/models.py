6a2b84ff1159c2746d135e1781130f1808e41568
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-12-22 11:02:18 +0000

    add model and DBD asset

--- src/models.py
+++ src/models.py
@@ -1,32 +1,66 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+# We use declarative_base() from SQLAlchemy to create a base class (Base)
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# We create a class (1 class = 1 table) that will inherit from the base class. Naming convention: PascalCase, plural:
+class Users(Base):
+    # 1. We create the table alias __tablename__. Naming convention: snake_case
+    __tablename__ = "users"
+    # 2. We define the columns of the table:
+    # 2.1. We define the primary key, with data type,primary_key=True
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    # 2.2. We define the model attributes, with data type
+    email = Column(String, unique=True)
+    username = Column(String(50),nullable=False)
+    firstname = Column(String(50),nullable=False)
+    lastname = Column(String(50),nullable=False)
+    image_url = Column(String(150))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Followers(Base):
+    __tablename__ = "followers"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # 2.2. We define the model attributes, with data type: no attributes here
+    # 2.3. We define the foreign key, with data type,ForeignKey("alias.id")
+    followers_id = Column(Integer, ForeignKey("users.id"))
+    following_id = Column(Integer, ForeignKey("users.id"))
+    # 3. We define the relationships: relationship(Models)
+    followers = relationship(Users)  # follower_id int fk >- users.id
+    following = relationship(Users)  # int fk >- users.id
 
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey("users.id"))
+    author = relationship(Users)  # author_id int fk >- users.id
+    
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    url = Column(String(150))
+    type = Column(Enum("image", "video"))
+    post_id = Column(Integer, ForeignKey("post.id"))
+    post = relationship(Post)  # post_id int fk >- post.id
+    
+class Comments(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(200))
+    author_id = Column(Integer, ForeignKey("users.id"))
+    post_id = Column(Integer, ForeignKey("post.id"))
+    author = relationship(Users)  # author_id int fk >- users.id
+    post = relationship(Post)  # post_id int fk >- post.id
+
+   
     def to_dict(self):
         return {}
+
+
 
 ## Draw from SQLAlchemy base
 try:
